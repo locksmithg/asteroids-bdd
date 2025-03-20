c5f2cabab30824f028c4a36d70836d46f62895e8
locksmithg <gabrielwsmith20@gmail.com>
2025-03-20 10:51:11 -0600

    asteroids, bullets, and so on

---
+++ asteroid.py
@@ -0,0 +1,20 @@
+from circleshape import CircleShape
+from constants import *
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.velocity = pygame.Vector2(0, 0)
+        self.speed = 100
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        return [Asteroid(self.position.x, self.position.y, self.radius//2),
+                Asteroid(self.position.x, self.position.y, self.radius//2)]
--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 import pygame
 from constants import *
+from player import Player
+from shot import Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    af = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        updatable.update(dt)
+        for a in asteroids:
+            if player.collides_with(a):
+                print("Game over!")
+                return
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
